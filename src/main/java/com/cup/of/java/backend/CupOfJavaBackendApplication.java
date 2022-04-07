9449e0f27e0e184cd77e7fd5f22f4d66179c51c6
vadim.burns <vadim.burns@protonmail.com>
2022-04-07 17:13:59 +0300

    init commit

---
+++ src/main/java/com/cup/of/java/backend/CupOfJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cup.of.java.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupOfJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupOfJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cup/of/java/backend/CupOfJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cup.of.java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupOfJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
